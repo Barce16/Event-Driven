1eb6e55034eecdcd4d02912e548f91641a841e56
unknown <gwapomark298@gmail.com>
2024-03-10 10:24:59 +0800

    Front-End-Development

--- index.php
+++ index.php
@@ -6,69 +6,76 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
+    <!-- link -->
     <script src="./assets/js/search.js"> </script>
+    <!-- link -->
 
+    <!-- link for Bootstrap -->
     <link href="./assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <script src="./assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- link for Bootstrap -->
 
-
+    <!-- backgournd -->
     <style>
         body {
             background-color: #ecf0f1;
         }
     </style>
-
+    <!-- backgournd -->
 </head>
 
 <body>
 
-
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <div class="container-fluid">
-            <span class="navbar-text">Welcome to Smart Reservation Parking System</span>
-        </div>
-    </nav>
-
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         <div class="container-fluid">
+            <!-- image -->
             <a class="navbar-brand" href="javascript:void(0)">
                 <img src="./assets/img/sport-car.png" height="50">
             </a>
+            <!-- image -->
+
+        <!-- buttons -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
                 <span class="navbar-toggler-icon"></span>
             </button>
+        <!-- buttons -->
+
+            <!-- Navigation bars front -->
             <div class="collapse navbar-collapse" id="mynavbar">
                 <ul class="navbar-nav me-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="/">Link</a>
+                        <a class="nav-link" href="javascript:void(0)">Dashboard</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="javascript:void(0)">Link</a>
+                        <a class="nav-link" href="./registration.php">Reservation</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="javascript:void(0)">Link</a>
+                        <a class="nav-link" href="./reservation.php">Reservation</a>
                     </li>
                 </ul>
+                <span class="navbar-text col text-center">Welcome to Smart Reservation Parking System</span>
                 <form class="d-flex">
                     <input onkeyup="search(this.value)" class="form-control me-2" type="text"
                         placeholder="Search anything here">
                     <button class="btn btn-primary" type="button">Search</button>
                 </form>
             </div>
+            <!-- Navigation bars buttom-->
         </div>
     </nav>
-
-    <div class="container-fluid">
-
+    <!-- For Text/Typography Front -->
+    <div class="container-fluid mb-5">
+        <p class="h1 mt-2">Smart Parking System</p>
+        <p class="mt-2">You can Park your car for safety and avoid carjacking</p>
         <div class="card mt-2">
-            <div class="card-header"><-----List of Guest-----></div>
+            <div class="card-header" style="text-align : center;"><-----List of Guest-----></div>
             <div class="card-body"> <strong> </strong>
                 <table class="table table-bordered table-hover" ;>
                     <thead>
                         <tr>
                             <th width="100" style="text-align : center;">Guest Id</th>
-                            <th style="text-align: left; padding-left: 10px">ID</th>
-                            <th width="130">Action</th>
+                            <th style="text-align: left; padding-left: 10px">Name</th>
+                            <th width="130">Details</th>
                         </tr>
                         <div class="modal" id="show-details">
                             <div class="modal-dialog">
@@ -76,7 +83,7 @@
 
                                     <!-- Modal Header -->
                                     <div class="modal-header">
-                                        <h4 class="modal-title">Modal Heading</h4>
+                                        <h4 class="modal-title">Guest Details</h4>
                                         <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                                     </div>
 
@@ -105,10 +112,12 @@
         </div>
         <div class="card-footer">This are all the List of Guest Park thier car to our Smart Parking System </div>
     </div>
-
+    <!-- For Text/Typography Buttom-->
 
 </body>
+<!-- jquery -->
 <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
     crossorigin="anonymous"></script>
+<!-- jquery -->
 
 </html>
--- model/searchEngine.php
+++ model/searchEngine.php
@@ -1,15 +1,15 @@
 <?php
 include('../config/database.php');
 
-$value = $_POST['search'];
-
+$value = $_POST['search'];// data that connect to function search.js
 $sql = "SELECT * FROM guest Where (Fname LIKE '%$value%' OR Guest_ID  LIKE '%$value%')";
-$result = $conn->query($sql);
+$result = $conn->query($sql); //connected to search.js
 
 if ($result->num_rows > 0) {
 
     while ($row = $result->fetch_assoc()) {
         ?>
+        <!-- database -->
         <tr>
             <td style="text-align : center;">
                 <?= $row['Guest_ID'] ?>
@@ -17,12 +17,15 @@ if ($result->num_rows > 0) {
             <td>
                 <?= $row['Fname'] ?>
             </td>
+        <!-- database -->
 
+        <!-- Buttons -->
             <td class="d-grid">
                 <button type="button" class="btn btn-sm btn-block
             btn-link" data-bs-toggle="modal" data-bs-target="#show-details">
                     show detail</button>
             </td>
+        <!-- database -->
         </tr>
         <?php
     }
